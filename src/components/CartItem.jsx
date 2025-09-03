67689c79f99c264b5a2722d69056808f3fbb6d5c
Soavin'i Avo RAKOTONIRINA <87423663+Soaviniavo@users.noreply.github.com>
2025-09-03 13:38:41 +0300

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { removeFromCart, updateQuantity } from "../features/cart/CartSlice";
+import { removeFromCart, updateQuantity } from "../features/cart/cartSlice";
 import { Minus, Plus, Trash2 } from "lucide-react";
 
 function CartItem({ item }) {
