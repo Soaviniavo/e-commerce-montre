f88469f7062f5c93f5b77a42af574cda0d4edff1
Soavin'i Avo RAKOTONIRINA <87423663+Soaviniavo@users.noreply.github.com>
2025-09-07 11:42:02 +0300

    Fix: product image path

--- src/Products.js
+++ src/Products.js
@@ -1,3 +1,27 @@
+import Rolex_Submariner from "./assets/rollexsibmariner.jpeg"
+import Tag_Heuer_Monaco from "./assets/tagHeuer.jpg"
+import Apple_Watch_Series_8 from "./assets/apple-watch-s8-caseback.jpg"
+import Samsung_Galaxy_Watch_6 from "./assets/samsung-galaxy-watch-6-classic-face-1.jpg"
+import Seiko_Presage from "./assets/seikoPresage.jpg"
+import Omega_Speedmaster from "./assets/omega-speedmaster-professional-2021-8-1-1200x675.jpg"
+
+
+import Casio_G_Shock from "./assets/casiogshock.jpg"
+import Fossil_Hybrid_HR from "./assets/fossil-hybrid-hr-review-going-the-pebble-way_4uky.1248.jpg"
+import Swatch_Irony from "./assets/90s-swatch-irony-my-happy-joe-collection-v0-x8b2n022b9bc1.jpg"
+import Citizen_Eco_Drive from "./assets/citizen-eco-drive-gold-date-dress-mens-watch-bm7332-61p-245723.jpeg"
+import Breitling_Navitimer from "./assets/breitling.jpg"
+import Tissot_PRX from "./assets/tissot-t1372071135100-prx-powermatic-80-15520256.jpg"
+
+import Cartier_Ballon_Bleu from "./assets/cartier.jpg"
+import Michael_Kors_Runway from "./assets/mk6674-michael-kors-watch-women-pyramid-crystals-white-dial-metal-stainless-steel-rose-gold-strap-quartz-battery-analog-three-hand-runway_16.webp"
+import Casio_F91W from "./assets/casioF91W.jpg"
+import Fossil_Jacqueline from "./assets/FossilJac.jpg"
+
+   
+    
+     
+     
 const Products = [
     {
         id: 1,
@@ -5,7 +29,7 @@ const Products = [
         price: 8000,
         category: "Luxury",
         description: "Iconic diving watch combining performance and luxury.",
-        image: "/src/assets/rollexsibmariner.jpeg"
+        image: Rolex_Submariner
     },
     {
         id: 2,
@@ -13,7 +37,7 @@ const Products = [
         price: 3500,
         category: "Luxury",
         description: "Vintage-style chronograph watch with a square case.",
-        image: "/src/assets/tagHeuer.jpg"
+        image: Tag_Heuer_Monaco
     },
     {
         id: 3,
@@ -21,7 +45,7 @@ const Products = [
         price: 450,
         category: "Smart",
         description: "Smartwatch with health tracking and smart notifications.",
-        image: "/src/assets/apple-watch-s8-caseback.jpg"
+        image: Apple_Watch_Series_8
     },
     {
         id: 4,
@@ -29,7 +53,7 @@ const Products = [
         price: 350,
         category: "Smart",
         description: "Durable smartwatch with AMOLED display and fitness tracking.",
-        image: "/src/assets/samsung-galaxy-watch-6-classic-face-1.jpg"
+        image: Samsung_Galaxy_Watch_6
     },
     {
         id: 5,
@@ -37,7 +61,7 @@ const Products = [
         price: 600,
         category: "Automatic",
         description: "Automatic watch with a classic mechanical movement.",
-        image: "/src/assets/seikoPresage.jpg"
+        image: Seiko_Presage
     },
     {
         id: 6,
@@ -45,15 +69,17 @@ const Products = [
         price: 6000,
         category: "Classic",
         description: "Legendary chronograph watch designed for space missions.",
-        image: "/src/assets/omega-speedmaster-professional-2021-8-1-1200x675.jpg"
+        image: Omega_Speedmaster
     },
+
+   
     {
         id: 7,
         title: "Casio G-Shock",
         price: 100,
         category: "Sport",
         description: "Shock-resistant watch, perfect for outdoor activities.",
-        image: "/src/assets/casiogshock.jpg"
+        image: Casio_G_Shock
     },
     {
         id: 8,
@@ -61,7 +87,7 @@ const Products = [
         price: 200,
         category: "Smart",
         description: "Hybrid watch with activity tracking and long battery life.",
-        image: "/src/assets/fossil-hybrid-hr-review-going-the-pebble-way_4uky.1248.jpg"
+        image: Fossil_Hybrid_HR
     },
     {
         id: 9,
@@ -69,7 +95,7 @@ const Products = [
         price: 150,
         category: "Classic",
         description: "Modern design classic watch with a metal case.",
-        image: "/src/assets/90s-swatch-irony-my-happy-joe-collection-v0-x8b2n022b9bc1.jpg"
+        image: Swatch_Irony
     },
     {
         id: 10,
@@ -77,7 +103,7 @@ const Products = [
         price: 300,
         category: "Quartz",
         description: "Quartz watch with solar technology, no battery needed.",
-        image: "/src/assets/citizen-eco-drive-gold-date-dress-mens-watch-bm7332-61p-245723.jpeg"
+        image: Citizen_Eco_Drive
     },
     {
         id: 11,
@@ -85,7 +111,7 @@ const Products = [
         price: 7000,
         category: "Men",
         description: "Iconic pilot watch featuring chronograph functions and masculine design.",
-        image: "/src/assets/breitling.jpg"
+        image: Breitling_Navitimer
     },
     {
         id: 12,
@@ -93,15 +119,16 @@ const Products = [
         price: 500,
         category: "Men",
         description: "Sporty and elegant watch with Swiss automatic movement.",
-        image: "/src/assets/tissot-t1372071135100-prx-powermatic-80-15520256.jpg"
+        image: Tissot_PRX
     },
+    
     {
         id: 13,
         title: "Cartier Ballon Bleu",
         price: 4000,
         category: "Women",
         description: "Elegant and refined watch with a rounded case and stainless steel finish.",
-        image: "/src/assets/cartier.jpg"
+        image: Cartier_Ballon_Bleu
     },
     {
         id: 14,
@@ -109,7 +136,7 @@ const Products = [
         price: 250,
         category: "Women",
         description: "Fashion watch with a gold dial and sparkling crystals, perfect for special occasions.",
-        image: "/src/assets/mk6674-michael-kors-watch-women-pyramid-crystals-white-dial-metal-stainless-steel-rose-gold-strap-quartz-battery-analog-three-hand-runway_16.webp"
+        image: Michael_Kors_Runway
     },
     {
         id: 15,
@@ -117,7 +144,7 @@ const Products = [
         price: 25,
         category: "Men",
         description: "Simple and durable digital watch with alarm and light functions.",
-        image: "/src/assets/casioF91W.jpg"
+        image: Casio_F91W
     },
     {
         id: 16,
@@ -125,7 +152,7 @@ const Products = [
         price: 150,
         category: "Women",
         description: "Delicate and elegant design watch with leather strap and minimalist dial.",
-        image: "/src/assets/FossilJac.jpg"
+        image: Fossil_Jacqueline
     }
 ];
 
